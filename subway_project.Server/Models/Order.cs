f2f43aa60d4128dcd3f132bceb429cd711f158fe
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-03-28 12:57:12 +0100

    Added Category, Order, Product, and Queue entities for database.

---
+++ subway_project.Server/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace subway_project.Server.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<Product> Products { get; set; } = new();
+    }
+}
---
+++ subway_project.Server/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace subway_project.Server.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public required Category Category { get; set; }
+        public string? ImageUrl { get; set; }
+        public List<Order> Orders { get; set; } = new();
+    }
+}
