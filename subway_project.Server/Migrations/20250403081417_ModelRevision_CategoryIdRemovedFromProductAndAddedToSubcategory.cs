58eb6aba9306291f252eea7133367bc81ef14ef5
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-04-03 10:16:32 +0200

    Migration for model update (CategoryId removed from Product and added to SubCategory.

---
+++ subway_project.Server/Migrations/20250403081417_ModelRevision_CategoryIdRemovedFromProductAndAddedToSubcategory.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace subway_project.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelRevision_CategoryIdRemovedFromProductAndAddedToSubcategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "SubCategories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubCategories_CategoryId",
+                table: "SubCategories",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubCategories_Categories_CategoryId",
+                table: "SubCategories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubCategories_Categories_CategoryId",
+                table: "SubCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SubCategories_CategoryId",
+                table: "SubCategories");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "SubCategories");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
