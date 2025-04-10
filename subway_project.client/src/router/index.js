e397a08bac132f54aa06e0f639d629d68186a93e
Markus Leion <markus.leion@gmail.com>
2025-04-10 23:58:06 +0200

    Revert "Add routes and components for kitchen and customer views"
    This reverts commit baf1731a4f10ebe37cf99f0ae3504c16aa85ee99.

--- subway_project.client/src/router/index.js
+++ subway_project.client/src/router/index.js
@@ -1,23 +1,17 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import StartPage from '@/views/StartPage.vue'
 import OrderPage from '@/views/OrderPage.vue'
-import KitchenPageVue from '@/views/KitchenViewPage.vue';
-import CustomerOrderListPageVue from '@/views/CustomerOrderViewPage.vue';
 
 const routes = [
-  { path: '/', name: 'StartPage', component: StartPage },
-  { path: '/order', name: 'OrderPage', component: OrderPage },
-  { path: '/kitchen', name: 'KitchenViewPage', component: KitchenPageVue },
-  { path: '/customer', name: 'CustomerOrderViewPage', component: CustomerOrderListPageVue },
+  {path: '/', name: 'StartPage', component: StartPage},
+  {path: '/order', name: 'OrderPage', component: OrderPage},
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', name: 'StartPage', component: StartPage },
-    { path: '/order', name: 'OrderPage', component: OrderPage },
-    { path: '/kitchen', name: 'KitchenPageView', component: KitchenPageVue },
-    { path: '/customer', name: 'CustomerOrderViewPage', component: CustomerOrderListPageVue },
+    {path: '/', name: 'StartPage', component: StartPage},
+  {path: '/order', name: 'OrderPage', component: OrderPage},
   ]
 });
 
