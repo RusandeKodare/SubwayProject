9f7114b2ce4a640d50764daa686f328d660e4955
PerssonITHS <persson999@gmail.com>
2025-04-10 13:26:29 +0200

    Add CartNav component to OrderPage for displaying cart items and total

---
+++ subway_project.client/src/components/CartNav.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+</script>
+
+<template>
+  <div class="cart">
+    <div class="cart-header">
+      <span>Your Order</span>
+    </div>
+
+    <div v-for="item in items" :key="item.id" class="cart-item">
+      <span>{{ item.name }}</span>
+      <span>{{ item.price }} kr</span>
+    </div>
+
+    <div class="cart-footer">
+      <strong>Totalt: {{ total }} kr</strong>
+      <button @click="checkout">Till kassan</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cart {
+  position: fixed;
+  top: 150px;
+  bottom: 150px;
+  right: 330px;
+  width: 250px;
+  height: 500px;
+  background-color: #015643;
+  box-shadow: -2px 0 8px rgba(0, 0, 0.1, 0.1);
+  padding: 20px;
+  overflow-y: auto;
+  z-index: 1000;
+  transition: transform 0.3s ease-in-out;
+  transform: translateX(100%);
+  border-radius: 10px;
+  font-family: "Kanit", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  color: white;
+}
+.cart-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-bottom: 20px;
+  text-decoration-line: underline;
+  text-decoration-color: yellow;
+  font-size: 20px;
+}
+.cart-item {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 15px;
+  border-bottom: 1px solid #eee;
+  padding-bottom: 10px;
+}
+.cart-footer {
+  margin-top: 20px;
+  text-align: right;
+}
+
+.cart-footer button {
+  background-color: #F4C12C;
+  color: #215B35;
+  padding: 10px 15px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  font-size: 15px;
+}
+
+.cart-footer button:hover {
+  background-color: #f7d46a;
+}
+</style>
