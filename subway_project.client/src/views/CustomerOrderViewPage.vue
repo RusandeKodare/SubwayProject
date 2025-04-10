955df5423a5b6af229119fd8e6776879efe3d446
Markus Leion <markus.leion@gmail.com>
2025-04-10 23:53:48 +0200

    Add routes and components for kitchen and customer views
    Updated `index.js` to include routes for `KitchenViewPage` and `CustomerOrderViewPage`. Created `CustomerOrderViewPage.vue` and `KitchenViewPage.vue` with setup scripts and templates for navigation and content display.

---
+++ subway_project.client/src/views/CustomerOrderViewPage.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+  import "../assets/main.css";
+  import { ref } from "vue";
+  import { useRouter } from "vue-router";
+  import TopNav from "@/components/TopNav.vue";
+  import CustomerOrderListPage from "@/components/CustomerOrderListPage.vue";
+
+</script>
+
+<template>
+
+  <div class="page-container">
+    <nav>
+      <TopNav />
+    </nav>
+    <section>
+      <CustomerOrderListPage />
+    </section>
+  </div>
+
+</template>
+
+<style>
+</style>
