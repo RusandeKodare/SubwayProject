99617406f3630847b76c25d5eb65ebeab918ec36
RusandeKodare <o.p.j@hotmail.com>
2025-04-13 10:00:38 +0200

    Update DbSeeder to add products and seed orders
    - Enhanced `DbSeeder.cs` to include new categories, subcategories, and product entries with updated image URLs.
    - Added functionality to seed orders into the database if none exist, capturing details like takeout status, total price, and order timestamps.

--- subway_project.Server/DbSeeder.cs
+++ subway_project.Server/DbSeeder.cs
@@ -8,6 +8,8 @@ namespace subway_project.Server
         public static void Seed(AppDbContext context)
         {
 
+
+
             if (!context.Categories.Any())
             {
                 context.Categories.AddRange(
@@ -29,12 +31,12 @@ namespace subway_project.Server
                     new SubCategory { Name = "Sauces", CategoryId = 1 },
                     new SubCategory { Name = "Cheese", CategoryId = 1 },
                     new SubCategory { Name = "Proteins", CategoryId = 1 },
-                    new SubCategory { Name = "Carbonated", CategoryId = 2},
-                    new SubCategory { Name = "Non Carbonated", CategoryId = 2},
-                    new SubCategory { Name = "Chips", CategoryId = 3},
-                    new SubCategory { Name = "Cookies", CategoryId = 3},
-                    new SubCategory { Name = "Ice-Cream", CategoryId = 4},
-                    new SubCategory { Name = "Pies", CategoryId = 4}
+                    new SubCategory { Name = "Carbonated", CategoryId = 2 },
+                    new SubCategory { Name = "Non Carbonated", CategoryId = 2 },
+                    new SubCategory { Name = "Chips", CategoryId = 3 },
+                    new SubCategory { Name = "Cookies", CategoryId = 3 },
+                    new SubCategory { Name = "Ice-Cream", CategoryId = 4 },
+                    new SubCategory { Name = "Pies", CategoryId = 4 }
                     );
 
                 context.SaveChanges();
@@ -49,12 +51,12 @@ namespace subway_project.Server
                     new Product { Name = "Whole Wheat Bread", Price = 20.00m, SubCategoryId = 1, ImageUrl = "https://okfrozendough.com/wp-content/uploads/2019/10/OK345_9_inc_ww_sub_bun.jpg" },
                     new Product { Name = "Light Bread", Price = 18.00m, SubCategoryId = 1, ImageUrl = "https://www.seasonsandsuppers.ca/wp-content/uploads/2022/09/sub-rolls-4-3-3.jpg" },
                     new Product { Name = "Cheese Bread", Price = 33.00m, SubCategoryId = 1, ImageUrl = "https://cdn.vox-cdn.com/thumbor/v5MOxdUdtffU4E1zkMM80jetZ2c=/0x0:1080x1350/1200x800/filters:focal(477x802:649x974)/cdn.vox-cdn.com/uploads/chorus_image/image/72447167/358101373_292422893350341_630677683818333789_n.0.jpg" },
-                    
+
 
                     new Product { Name = "Tomato", Price = 5.00m, SubCategoryId = 2, ImageUrl = "https://m.economictimes.com/thumb/msid-95423731,width-1200,height-900,resizemode-4,imgsize-56196/tomatoes-canva.jpg" },
-                    new Product { Name = "Onion", Price = 3.00m, SubCategoryId = 2, ImageUrl= "https://ih1.redbubble.net/image.3804311069.0627/raf,360x360,075,t,fafafa:ca443f4786.jpg" },
-                    new Product { Name = "Bell Pepper", Price = 6.00m, SubCategoryId = 2, ImageUrl= "https://www.chilipeppermadness.com/wp-content/uploads/2024/02/Bell-Peppers1.jpg" },
-                    new Product { Name = "Cucumber", Price = 4.00m, SubCategoryId = 2, ImageUrl= "https://snaped.fns.usda.gov/sites/default/files/styles/crop_ratio_7_5/public/seasonal-produce/2018-05/cucumbers2.jpg.webp?itok=Y2CZseBE" },
+                    new Product { Name = "Onion", Price = 3.00m, SubCategoryId = 2, ImageUrl = "https://ih1.redbubble.net/image.3804311069.0627/raf,360x360,075,t,fafafa:ca443f4786.jpg" },
+                    new Product { Name = "Bell Pepper", Price = 6.00m, SubCategoryId = 2, ImageUrl = "https://www.chilipeppermadness.com/wp-content/uploads/2024/02/Bell-Peppers1.jpg" },
+                    new Product { Name = "Cucumber", Price = 4.00m, SubCategoryId = 2, ImageUrl = "https://snaped.fns.usda.gov/sites/default/files/styles/crop_ratio_7_5/public/seasonal-produce/2018-05/cucumbers2.jpg.webp?itok=Y2CZseBE" },
                     new Product { Name = "Spinach", Price = 5.00m, SubCategoryId = 2, ImageUrl = "https://bedford.tennessee.edu/wp-content/uploads/sites/162/2020/08/spinach-leaves-1024x593.jpg" },
 
                     new Product { Name = "Mayonnaise", Price = 5.00m, SubCategoryId = 3, ImageUrl = "https://media.istockphoto.com/id/507636140/photo/homemade-mayonnaise.jpg?s=612x612&w=0&k=20&c=Yo39cXtQ0x4uRJbRKMo__9ppA-BKcLI82PbMcn4aweo=" },
@@ -64,19 +66,19 @@ namespace subway_project.Server
                     new Product { Name = "Ranch Sauce", Price = 5.00m, SubCategoryId = 3, ImageUrl = "https://taoofspice.com/wp-content/uploads/2022/01/Close-up-of-garlic-ranch-dip-with-carrots-surrounding-it-683x1024.jpg.webp" },
 
 
-                    new Product { Name = "Cheddar", Price = 10.00m, SubCategoryId = 4, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
-                    new Product { Name = "Mozzarella", Price = 12.00m, SubCategoryId = 4, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
-                    new Product { Name = "Hard Cheese", Price = 14.00m, SubCategoryId = 4, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
+                    new Product { Name = "Cheddar", Price = 10.00m, SubCategoryId = 4, ImageUrl = "https://images3.alphacoders.com/665/665670.jpg" },
+                    new Product { Name = "Mozzarella", Price = 12.00m, SubCategoryId = 4, ImageUrl = "https://www.shutterstock.com/shutterstock/videos/3391081405/thumb/3.jpg?ip=x480" },
+                    new Product { Name = "Hard Cheese", Price = 14.00m, SubCategoryId = 4, ImageUrl = "https://www.shutterstock.com/shutterstock/videos/3665506537/thumb/1.jpg?ip=x480" },
 
-                    new Product { Name = "Ham", Price = 15.00m, SubCategoryId = 5, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
-                    new Product { Name = "Turkey", Price = 20.00m, SubCategoryId = 5, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
-                    new Product { Name = "Beef", Price = 30.00m, SubCategoryId = 5, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
-                    new Product { Name = "Chicken", Price = 25.00m, SubCategoryId = 5, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
-                    new Product { Name = "Vegetarian", Price = 18.00m, SubCategoryId = 5, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
-                    new Product { Name = "Tuna", Price = 22.00m, SubCategoryId = 5, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
-                    new Product { Name = "Chicken Breast", Price = 25.00m, SubCategoryId = 5, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
-                    new Product { Name = "Steak", Price = 30.00m, SubCategoryId = 5, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
-                    new Product { Name = "Tofu", Price = 18.00m, SubCategoryId = 5, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
+                    new Product { Name = "Ham", Price = 15.00m, SubCategoryId = 5, ImageUrl = "https://www.shutterstock.com/shutterstock/videos/1105399823/thumb/1.jpg?ip=x480" },
+                    new Product { Name = "Turkey", Price = 20.00m, SubCategoryId = 5, ImageUrl = "https://t3.ftcdn.net/jpg/02/87/06/90/360_F_287069085_cSzPLAoux5MSAeETX4Pwhdxa5gxrbqf4.jpg" },
+                    new Product { Name = "Beef", Price = 30.00m, SubCategoryId = 5, ImageUrl = "https://media.istockphoto.com/id/535786572/photo/grilled-striploin-steak.jpg?s=612x612&w=0&k=20&c=F_vrvwIOWe3vBR2y16Dp4z6d46K1sIY3togU3VYjrpA=" },
+                    new Product { Name = "Chicken", Price = 25.00m, SubCategoryId = 5, ImageUrl = "https://www.everyday-delicious.com/wp-content/uploads/2021/03/chicken-spread-pasta-z-kurczaka-everyday-delicious-1.jpg" },
+                    new Product { Name = "Vegetarian", Price = 18.00m, SubCategoryId = 5, ImageUrl = "https://cheapandcheerfulcooking.com/wp-content/uploads/2021/02/muhammara-red-pepper-walnut-dip-1.jpg" },
+                    new Product { Name = "Tuna", Price = 22.00m, SubCategoryId = 5, ImageUrl = "https://www.goodnesst.com/wp-content/uploads/2019/12/easy-tuna-spread-recipe-2.jpg" },
+                    new Product { Name = "Chicken Breast", Price = 25.00m, SubCategoryId = 5, ImageUrl = "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2023/01/Air-Fryer-Chicken-Breast-7.jpg" },
+                    new Product { Name = "Steak", Price = 30.00m, SubCategoryId = 5, ImageUrl = "https://i.redd.it/03zxsyqc6otb1.jpg" },
+                    new Product { Name = "Tofu", Price = 18.00m, SubCategoryId = 5, ImageUrl = "https://alegumeaday.com/wp-content/uploads/2021/08/Creamy-sun-dried-tomato-tofu-spread-A-Legume-a-Day-20210805-1.jpg" },
 
                     new Product { Name = "Coca-Cola", Price = 15.00m, SubCategoryId = 6, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
                     new Product { Name = "Fanta", Price = 15.00m, SubCategoryId = 6, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
@@ -95,6 +97,48 @@ namespace subway_project.Server
                     new Product { Name = "Apple Pie", Price = 18.00m, SubCategoryId = 11, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" },
                     new Product { Name = "Cheesecake", Price = 22.00m, SubCategoryId = 11, ImageUrl = "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=445&dpr=1&s=none&crop=none" }
                 );
+
+                context.SaveChanges();
+            }
+
+            if (!context.Orders.Any())
+            {
+                context.Orders.AddRange(
+                 
+                    new Order
+                    {
+                        TakeAway = true,
+                        TotalPrice = 300,
+                        Products = new List<Product> { context.Products.FirstOrDefault(p => p.Name == "White Bread")!, context.Products.FirstOrDefault(p => p.Name == "Tomato")!, context.Products.FirstOrDefault(p => p.Name == "Mayonnaise")! },
+                        OrderReceived = DateTime.Now.AddMinutes(10)
+                    },
+                    new Order
+                    {
+                        TakeAway = false,
+                        TotalPrice = 300,
+                        Products = new List<Product> { context.Products.FirstOrDefault(p => p.Name == "White Bread")!, context.Products.FirstOrDefault(p => p.Name == "Tomato")!, context.Products.FirstOrDefault(p => p.Name == "Mayonnaise")! },
+                        OrderReceived = DateTime.Now,
+                        OrderInProgress = DateTime.Now.AddMinutes(5)
+                    },
+                    new Order
+                    {
+                        TakeAway = false,
+                        TotalPrice = 300,
+                        Products = new List<Product> { context.Products.FirstOrDefault(p => p.Name == "White Bread")!, context.Products.FirstOrDefault(p => p.Name == "Tomato")!, context.Products.FirstOrDefault(p => p.Name == "Mayonnaise")! },
+                        OrderReceived = DateTime.Now,
+                        OrderInProgress = DateTime.Now.AddMinutes(5),
+                        OrderCompleted = DateTime.Now.AddMinutes(15)
+                    },
+                    new Order
+                    {
+                        TakeAway = true,
+                        TotalPrice = 300,
+                        Products = new List<Product> { context.Products.FirstOrDefault(p => p.Name == "White Bread")!, context.Products.FirstOrDefault(p => p.Name == "Tomato")!, context.Products.FirstOrDefault(p => p.Name == "Mayonnaise")! },
+                        OrderReceived = DateTime.Now,
+                        OrderInProgress = DateTime.Now.AddMinutes(5),
+                        OrderCompleted = DateTime.Now.AddMinutes(15),
+                        OrderCollected = DateTime.Now.AddMinutes(16)
+                   });
 
                 context.SaveChanges();
             }
