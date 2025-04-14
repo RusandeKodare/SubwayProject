e25a83de179ed08d569badf27b2188321805a65e
RusandeKodare <o.p.j@hotmail.com>
2025-04-14 12:06:04 +0200

    Refactor ProductPostDTO to use SubCategoryId
    - Removed CategoryId and added SubCategoryId property.

--- subway_shared/DTOs/ProductDTOs/ProductPostDTO.cs
+++ subway_shared/DTOs/ProductDTOs/ProductPostDTO.cs
@@ -4,6 +4,6 @@ public class ProductPostDTO
 {
 	public required string Name { get; set; }
 	public decimal Price { get; set; }
-	public int CategoryId { get; set; }
+	public int SubCategoryId { get; set; }
 	public string ImageUrl { get; set; } = "images\\no-image-available.jpg";
 }
