17701df4e946becc553e3508550390121a779c21
RusandeKodare <o.p.j@hotmail.com>
2025-04-14 12:03:52 +0200

    Add MappingProfile for AutoMapper configurations
    - Introduces a new `MappingProfile` class to set up mappings between `ProductPostDTO` and `Product`, and `OrderDTO` and `Order`. Includes necessary using directives for AutoMapper and related DTO and model classes.

---
+++ subway_project.Server/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using subway_project.Server.Models;
+using subway_shared.DTOs.OrderDTOs;
+using subway_shared.DTOs.ProductDTOs;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<ProductPostDTO, Product>();
+        CreateMap<OrderDTO, Order>();
+    }
+}
