7f850a98a25a34ad185af267eb84487933b36493
PerssonITHS <persson999@gmail.com>
2025-04-23 09:38:36 +0200

    Implement removeProduct action in useOrderStore for product removal from order

--- subway_project.client/src/stores/useOrderStore.js
+++ subway_project.client/src/stores/useOrderStore.js
@@ -22,6 +22,15 @@ export const useOrderStore = defineStore('order', {
         this.order.totalPrice += price;
         console.log('PINIA: price added: ', price, 'new total: ', this.order.totalPrice)
     },
+    removeProduct(product){
+        const index = this.order.products.findIndex(p => p.id === product.id);
+        if (index !== -1) {
+            this.order.products.splice(index, 1);
+            console.log('PINIA: removed product', product, 'from order:', this.order.products)
+        } else {
+            console.log('PINIA: product not found in order:', product)
+        }
+    },
     resetOrder(){
         this.order = {
             takeAway: false,
