963c1b7330cff896d4c9d132bb19f512ae0155e7
RusandeKodare <o.p.j@hotmail.com>
2025-04-24 14:37:40 +0200

    Add order management features and improve error handling
    - Introduced `ReturnOrderId` function in `OrdersStore.js` to fetch order by customer ID.
    - Updated return object in `OrdersStore` to include `ReturnOrderId`.
    - Added `uuidv4` import in `useOrderStore.js` for unique customer IDs.
    - Modified order state to include `customerId` initialized with `uuidv4()`.
    - Updated `removeProduct` method to adjust total price on product removal.
    - Enhanced logging in `changeTakeAway` and `addToTotalPrice` methods.

--- subway_project.client/src/stores/useOrderStore.js
+++ subway_project.client/src/stores/useOrderStore.js
@@ -1,41 +1,54 @@
 import { defineStore } from 'pinia'
+import { v4 as uuidv4 } from 'uuid';
 
 export const useOrderStore = defineStore('order', {
   state: () => ({
     order: {
         takeAway: false,
         totalPrice: 0,
-        products: []
+        products: [],
+        customerId: uuidv4(),
+
     }
   }),
 
   actions: {
     addProduct(product){
         this.order.products.push(product);
+        this.order.totalPrice += product.price;
+        console.log('PINIA: price added: ', product.price, 'new total: ', this.order.totalPrice)
         console.log('PINIA: added product', product, 'to order:', this.order.products)
     },
-    changeTakeAway(boolean){
-        this.order.takeAway = boolean;
-        console.log('PINIA: changed take away to:', boolean)
-    },
-    addToTotalPrice(price){
-        this.order.totalPrice += price;
-        console.log('PINIA: price added: ', price, 'new total: ', this.order.totalPrice)
-    },
+
     removeProduct(product){
         const index = this.order.products.findIndex(p => p.id === product.id);
         if (index !== -1) {
             this.order.products.splice(index, 1);
+            this.order.totalPrice -= product.price;
+            console.log('PINIA: removed price: ', product.price, 'new total: ', this.order.totalPrice)
             console.log('PINIA: removed product', product, 'from order:', this.order.products)
         } else {
             console.log('PINIA: product not found in order:', product)
         }
     },
+
+    changeTakeAway(boolean){
+        this.order.takeAway = boolean;
+        console.log('PINIA: changed take away to:', boolean)
+        console.log('Total price', this.order.totalPrice)
+    },
+
+    addToTotalPrice(price){
+        this.order.totalPrice += price;
+        console.log('PINIA: price added: ', price, 'new total: ', this.order.totalPrice)
+    },
+
     resetOrder(){
         this.order = {
             takeAway: false,
             totalPrice: 0,
-            products: []
+            products: [],
+            customerId: uuidv4(),
         }
         console.log('PINIA: order has been reset')
     }
