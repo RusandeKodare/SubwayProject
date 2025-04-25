a5455d11aa163d2100d53864eaf531882937b16d
RusandeKodare <o.p.j@hotmail.com>
2025-04-25 13:08:33 +0200

    Enhance product and order structures in DbSeeder
    - Added `CategoryId` to `Product` instances and introduced `Subs` property in `Order` instances for better order management.
    
    - Removed outdated `Order` instances.
    
    - Updated `MappingProfile` to include mapping for `SubDTO` to `Sub`, ignoring the `Products` property for improved DTO handling.

--- subway_project.Server/MappingProfile.cs
+++ subway_project.Server/MappingProfile.cs
@@ -2,6 +2,7 @@
 using subway_project.Server.Models;
 using subway_shared.DTOs.OrderDTOs;
 using subway_shared.DTOs.ProductDTOs;
+using subway_shared.DTOs.SubDTOs;
 
 public class MappingProfile : Profile
 {
@@ -9,5 +10,8 @@ public class MappingProfile : Profile
     {
         CreateMap<ProductPostDTO, Product>();
         CreateMap<OrderDTO, Order>();
+        CreateMap<SubDTO, Sub>()
+            .ForMember(dest => dest.Products, opt => opt.Ignore());
+
     }
 }
