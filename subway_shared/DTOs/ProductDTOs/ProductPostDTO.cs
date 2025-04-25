ec38c42822e95f532f97016c782754ccb676ad6a
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-04-25 16:26:06 +0200

    Added CategoryId to Product DTOs.

--- subway_shared/DTOs/ProductDTOs/ProductPostDTO.cs
+++ subway_shared/DTOs/ProductDTOs/ProductPostDTO.cs
@@ -5,6 +5,7 @@ public class ProductPostDTO
     public int Id { get; set; }
     public required string Name { get; set; }
 	public decimal Price { get; set; }
-	public int SubCategoryId { get; set; }
+    public int CategoryId { get; set; }
+    public int SubCategoryId { get; set; }
 	public string ImageUrl { get; set; } = "images\\no-image-available.jpg";
 }
