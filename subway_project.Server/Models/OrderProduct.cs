c3a4d792d97b2ce497ee7b0adce01218c0a739a1
RusandeKodare <o.p.j@hotmail.com>
2025-04-29 13:31:36 +0200

    Refactor Order class and introduce OrderProduct model
    - Updated the `Order` class to replace `Products` and `Subs` with an `OrderProducts` property of type `ICollection<OrderProduct>`, enhancing the structure of product associations.
    - Added a new `OrderProduct` class to represent the relationship between orders and products, including properties for `OrderId`, `ProductId`, and `Quantity`.

---
+++ subway_project.Server/Models/OrderProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace subway_project.Server.Models
+{
+    public class OrderProduct
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
