e332921ef8eb7143d01b93a0430359e11423afb0
RusandeKodare <o.p.j@hotmail.com>
2025-04-30 14:07:14 +0200

    Add Specials class to Server.Models namespace
    - Introduced a new `Specials` class with properties:
    - `Id`: an integer identifier
    - `OrderProducts`: a collection initialized as an empty array
    - `Subs`: a list initialized as a new list

---
+++ subway_project.Server/Models/Specials.cs
@@ -0,0 +1,9 @@
+﻿namespace subway_project.Server.Models
+{
+    public class Specials
+    {
+        public int Id { get; set; }
+        public ICollection<OrderProduct> OrderProducts { get; set; } = [];
+        public List<Sub> Subs { get; set; } = new();
+    }
+}
