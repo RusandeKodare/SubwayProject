9e39d491d997bbc01058755c5807f7816060ab90
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-04-30 14:41:39 +0200

    Migration having updated data model to allow multiple subs in one order.

--- subway_project.Server/Migrations/20250430124030_ModelRevision_MultipleSubsPerOrder.Designer.cs
+++ subway_project.Server/Migrations/20250430124030_ModelRevision_MultipleSubsPerOrder.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using subway_project.Server.Data;
 
@@ -11,9 +12,11 @@ using subway_project.Server.Data;
 namespace subway_project.Server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250430124030_ModelRevision_MultipleSubsPerOrder")]
+    partial class ModelRevision_MultipleSubsPerOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
