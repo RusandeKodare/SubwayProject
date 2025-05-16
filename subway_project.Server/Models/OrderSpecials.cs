eaf69ca009826e16c08fd84d6e034ca3b1a9d402
RusandeKodare <o.p.j@hotmail.com>
2025-05-16 13:17:43 +0200

    Add OrderSpecials class to Server.Models namespace
    - Introduced a new `OrderSpecials` class with properties
    `OrderId`, `SpecialId`, `Special` (nullable), and `Quantity`
    to manage order special details in the application.

---
+++ subway_project.Server/Models/OrderSpecials.cs
@@ -0,0 +1,10 @@
+﻿namespace subway_project.Server.Models
+{
+    public class OrderSpecials
+    {
+        public int OrderId { get; set; }
+        public int SpecialId { get; set; }
+        public Specials? Special { get; set; }
+        public int Quantity { get; set; }
+    }
+}
