fcdbb2b247a0e500f24f39b3cc59a2a3dc6221af
RusandeKodare <o.p.j@hotmail.com>
2025-05-16 13:19:19 +0200

    Add SpecialProduct class to Server.Models namespace
    - Introduced a new `SpecialProduct` class with properties SpecialId` and `ProductId`, along with navigation properties `Special` and `Product`.

---
+++ subway_project.Server/Models/SpecialProduct.cs
@@ -0,0 +1,12 @@
+﻿namespace subway_project.Server.Models
+{
+    public class SpecialProduct
+    {
+        public int SpecialId { get; set; }
+        public Specials Special { get; set; } = null!;
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+    }
+
+}
