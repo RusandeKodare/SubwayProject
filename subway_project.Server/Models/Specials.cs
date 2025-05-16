aa72de75f8750fa0d74d505091e9d21bb32c757b
RusandeKodare <o.p.j@hotmail.com>
2025-05-16 13:19:36 +0200

    Add Description and SpecialProducts to Specials class
    - Introduced a new `Description` property of type `string`.
    - Added a new `SpecialProducts` property of type `ICollection<SpecialProduct>`.
    - Retained the `ImageUrl` property with its default value.

--- subway_project.Server/Models/Specials.cs
+++ subway_project.Server/Models/Specials.cs
@@ -5,9 +5,12 @@
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public int Price { get; set; }
+        public string Description { get; set; } = null!;
         public int SubCategoryId { get; set; }
         public int CategoryId { get; set; }
         public string ImageUrl { get; set; } = "images\\no-image-available.jpg";
+        public ICollection<SpecialProduct> SpecialProducts { get; set; } = [];
+
 
     }
 }
